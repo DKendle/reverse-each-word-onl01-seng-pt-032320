0b9f9e778004758a56dc265c3bf98a419cd8ebcd
Dina Kendle <dina.kendle@gmail.com>
2020-04-02 15:21:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,12 @@
-def reverse_each_word(sentence)
-  reversed_word = Array.new()
-  sentence.split.each do |word|
-    reversed_word << "#{word.reverse}"
+#def reverse_each_word(sentence)
+  #reversed_word = Array.new()
+  #sentence.split.each do |word|
+   # reversed_word << "#{word.reverse}"
     
-  end
-    reversed_word.join(" ")
+ # end
+   # reversed_word.join(" ")
 end
 
+def reverse_each_word(sentence)
+  sentence.split.collect do |word|
+    "#{word.reverse}"
